cf15ce14a1d12b97fe1c127a6b8818fa97eb03c2
Luis Martinez <luis.martinez.sat@gmail.com>
2023-10-19 11:31:59 -0500

    added notes following review

--- subqueries_exercises.sql
+++ subqueries_exercises.sql
@@ -27,14 +27,14 @@ WHERE e.first_name IN (
 					WHERE first_name = 'Aamod')
 and t.to_date = '9999-01-01';
 -- ANSWER: 168 titles. 
-
+-- you can also use distinct to find just the titles that occur without duplicates (6 unique titles).  
 
 
 
 -- 3) How many people in the employees table are no longer working for the company? Give the answer in a comment in your code.
 SELECT COUNT(*)
 FROM employees
-WHERE emp_no NOT IN (SELECT emp_no
+WHERE emp_no NOT IN (SELECT emp_no  -- filtering out people who are current employees with NOT IN.
 					FROM salaries
 					WHERE to_date = '9999-01-01');
 -- Answer: '59,900'
@@ -65,8 +65,17 @@ JOIN salaries as s
 USING (emp_no)
 WHERE salary >(          
 				SELECT ROUND(AVG(salary))
-				FROM salaries) and s.to_date > NOW();
+				FROM salaries) 
+and s.to_date > NOW();
+                -- These are the names wihtout the count
                 
+                -- instructor's method \/
+select count(*)
+from salaries
+where to_date > now()
+and salary > (SELECT ROUND(AVG(salary), 2) -- this rounding will change the result (3 people can fall off if you round to whole number)
+				FROM salaries) ;
+                -- This is just the count (154,543) 
                 
                 
 	
@@ -82,6 +91,57 @@ WHERE salary > (
 				SELECT MAX(salary) - STDDEV(salary)
 				FROM salaries)
 and to_date > NOW();
+-- ANSWER 78 -- incorrect
+
+
+
+-- instructor's method
+select std(salary)
+from salaries
+where to_date > now();
+--  1 standard deviation (17309.95933634675)
+
+
+select max(salary)
+from salaries
+where to_date > now();
+-- current highest salary (158220)
+
+
+-- one option
+-- select *
+-- from salaries
+-- where salary between (    -- cutoff point and max salary
+-- )
+-- and (
+-- )
+
+-- or we can use salaries that are greater than or equal to the cut off point
+
+-- select *
+-- from salaries
+-- where salary >=    -- cutoff point
+
+
+
+select max(salary) - std(salary)  -- calculating my cutoff point (140910.04066365326)
+from salaries
+where to_date > now();
+
+
+
+select count(*)
+from salaries
+where salary >= (
+				select max(salary) - std(salary)  
+				from salaries
+				where to_date > now())
+and to_date > now();
+-- ANSWER: 83 current salaries within 1 sd from max salary
+
+
+
+
 
 -- Percentage of all salaries
 SELECT COUNT(*) / (SELECT COUNT(*) FROM salaries WHERE to_date > NOW()) *100
@@ -90,11 +150,32 @@ WHERE salary >= (
 				SELECT MAX(salary) - STDDEV(salary)
 				FROM salaries)
 AND to_date > NOW();
+-- ANSWER: 0.0325
 
+-- instructor's method
+
+select count(*) -- how many current total salaries (240124)
+from salaries
+where to_date > now();
                     
-                    
-                    
-                    
+			
+ select (  select count(*)
+from salaries
+where salary >= (
+				select max(salary) - std(salary)  
+				from salaries
+				where to_date > now())
+and to_date > now()) -- my 83 salaries
+
+/ (select count(*) -- how many current total salaries (240124)
+from salaries
+where to_date > now())
+
+* 100;
+
+               -- this is the same as doing select 83 / 240124 * 100;
+               
+-- ANSWER %0.0346
                     
                     
                     
